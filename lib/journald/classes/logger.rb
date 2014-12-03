63321bce5fcae69def640c7b0d174dd9bd459d27
Anton Smirnov <anton.smirnov@mkechinov.ru>
2014-12-03 20:35:14 +0300

    make includer

--- lib/journald/classes/logger.rb
+++ lib/journald/classes/logger.rb
@@ -1,13 +1,3 @@
-require 'logger'
-require 'journald/native'
-
-require_relative 'modules/version'
-require_relative 'modules/exceptionable'
-require_relative 'modules/loggable'
-require_relative 'modules/sysloggable'
-
-require_relative 'trace_logger'
-
 module Journald
   class Logger
     include Exceptionable
---
+++ lib/journald/logger.rb
@@ -0,0 +1,10 @@
+require 'logger'
+require 'journald/native'
+
+require_relative 'modules/version'
+require_relative 'modules/exceptionable'
+require_relative 'modules/loggable'
+require_relative 'modules/sysloggable'
+
+require_relative 'classes/logger'
+require_relative 'classes/trace_logger'
